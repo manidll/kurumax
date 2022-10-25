1216a8a1bd9eca68a0ae88d078ae165bb97d597b
mane0 <mane0@192.168.1.64>
2022-10-24 20:46:19 -0700

    COMO CHINGAS PEDRO

---
+++ src/mx/itson/kuruma/ui/MainTest.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mx.itson.kuruma.ui;
+
+/**
+ *
+ * @author pedro
+ */
+public class MainTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String moneda[] = {"Euro", "Dolar", "Yen", "Rublo", "Libra", "Quetzal", "Lira", "Won", "Boliviano" };
+        double cambio[] = {0.1500, 1.00, 0.00691, 0.01637, 1.11645, 0.12650, 0.05397, 0.69840, 0.14480};
+        
+        for (int i = 0; i == i; i++) {
+            System.out.println("Nombre de moneda: "+moneda[i]+"\t"+cambio[i]);
+        }
+    }
+    
+}
